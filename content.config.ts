4156c7d0f881a7a38417dafec559fc2ecb11a14d
LazTech <laznology.dev@gmail.com>
2026-04-05 20:39:20 +0700

    feat: add first content entry and update content schema

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,16 @@
-import { defineContentConfig, defineCollection } from '@nuxt/content'
+import { defineContentConfig, defineCollection, z } from '@nuxt/content'
 
 export default defineContentConfig({
   collections: {
     content: defineCollection({
       type: 'page',
-      source: '**',
-    }),
-  },
+      source: '**/*.md',
+      schema: z.object({
+        date: z.string(),
+        author: z.string(),
+        image: z.string().optional(),
+        tags: z.array(z.string()).default([])
+      })
+    })
+  }
 })
